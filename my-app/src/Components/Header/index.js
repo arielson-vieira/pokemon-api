48044d1da90fa71d57800e3afb85733dcd7da7c4
Arielson <aryelson17@gmail.com>
2023-01-25 20:54:19 -0300

    aplicação de Responsividade em CSS no header, home page e footer.

--- my-app/src/Components/Header/index.js
+++ my-app/src/Components/Header/index.js
@@ -53,26 +53,31 @@ const Header = (params) => {
   return (
     <header className="header">
       <div className="iconsLeft">
-        <img src={iconPokemon} onClick={goToHome} />
-
-        <Link to="/" onClick={goToTop}>
-          HOME
+       
+          <img src={iconPokemon} onClick={goToHome} />
+       
+        <Link to="/" onClick={goToTop} className="hiddenMobile">
+          INÍCIO
         </Link>
-        <Link to="/" onClick={goToTop} className='hiddenMobile'>
+        <Link to="/" onClick={goToTop} className="hiddenMobile">
           FILMES
         </Link>
-        {userConnect &&(
-        <Link to="/pokemons">POKEMONS</Link>
-      )}
+        {userConnect && <Link to="/pokemons">CARDS</Link>}
       </div>
       <div className="iconsRight">
         <div className="group">
+          <div className="hiddenMobile">
           <svg className="iconInput" aria-hidden="true" viewBox="0 0 24 24">
             <g>
               <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
             </g>
           </svg>
-          <input placeholder="Pesquisar" type="search" className="inputSearch" />
+            <input
+              placeholder="Pesquisar"
+              type="search"
+              className="inputSearch"
+            />
+          </div>
         </div>
         {userPublic && (
           <>
@@ -80,7 +85,7 @@ const Header = (params) => {
               <span className="buttonsHome">CONECTAR</span>
             </button>
             <button onClick={goToRegister}>
-              <span className="buttonsHome">REGISTRAR</span>
+              <span className="buttonsHome">CADASTRAR</span>
             </button>
           </>
         )}
@@ -88,7 +93,6 @@ const Header = (params) => {
           <div className="userConnected">
             <p>Username</p>
             <img src={IconUser} />
-            
           </div>
         )}
         <ToastContainer
